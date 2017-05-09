e746a7e619a13e0b65465ce22255971da36745b2
leassy <leassy@gmail.com>
2017-05-09 15:43:05 +0800

    Update shadowsocks-go.sh

--- shadowsocks/go/shadowsocks-go.sh
+++ shadowsocks/go/shadowsocks-go.sh
@@ -241,7 +241,7 @@ config_shadowsocks(){
     "server_port":${shadowsocksport},
     "local_port":1080,
     "password":"${shadowsockspwd}",
-    "method":"aes-256-cfb",
+    "method":"rc4-md5",
     "timeout":600
 }
 EOF
@@ -320,7 +320,7 @@ install(){
     echo -e "Your Server Port: \033[41;37m ${shadowsocksport} \033[0m"
     echo -e "Your Password: \033[41;37m ${shadowsockspwd} \033[0m"
     echo -e "Your Local Port: \033[41;37m 1080 \033[0m"
-    echo -e "Your Encryption Method: \033[41;37m aes-256-cfb \033[0m"
+    echo -e "Your Encryption Method: \033[41;37m rc4-md5 \033[0m"
     echo
     echo "Welcome to visit:http://xos.me/shadowsocks-go.html"
     echo "Enjoy it!"
